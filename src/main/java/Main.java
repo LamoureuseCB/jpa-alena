6744a31ae8b5f5b5e9d9c892161fb5d68e2ced74
LamoureuseCB <148466239+LamoureuseCB@users.noreply.github.com>
2024-10-16 22:49:29 +0500

    created headphones category

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,18 @@ import jakarta.persistence.*;
 import model.Category;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
         EntityManager manager = factory.createEntityManager();
+
+
 //        findAll
-        TypedQuery<Category> query = manager.createQuery("select c from Category c", Category.class);
-        List<Category> categoryList = query.getResultList();
-       categoryList.forEach(category -> System.out.println(categoryList));
+//        TypedQuery<Category> query = manager.createQuery("select c from Category c", Category.class);
+//        List<Category> categoryList = query.getResultList();
+//       categoryList.forEach(category -> System.out.println(categoryList));
 //
 ////       findById
 //        Category foundById = manager.find(Category.class,1);
@@ -21,18 +24,40 @@ public class Main {
 //        Category category = query.getSingleResult();
 
 //        insert
-        Category category1 = new Category();
-        category1.setName("Phones");
-        try{
-            manager.getTransaction().begin();
-            manager.persist(category1);
-            manager.getTransaction().commit();
-        }catch(Exception e){
-            manager.getTransaction().rollback();
-            System.out.println(e.getMessage());
+//        Category category1 = new Category();
+//        category1.setName("Phones");
+//        try{
+//            manager.getTransaction().begin();
+//            manager.persist(category1);
+//            manager.getTransaction().commit();
+//        }catch(Exception e){
+//            manager.getTransaction().rollback();
+//            System.out.println(e.getMessage());
+//
+//        }
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название категории:");
+        String categoryName = scanner.nextLine();
+
+        TypedQuery<Category> typedQuery = manager.createQuery(("select c from Category c where c.name = ?1"), Category.class);
+        typedQuery.setParameter(1, "Наушники");
+        List<Category> categories = typedQuery.getResultList();
+        if (categories.isEmpty()) {
+            Category headPhone = new Category();
+            headPhone.setName("Наушники");
+            try {
+                manager.getTransaction().begin();
+                manager.persist(headPhone);
+                manager.getTransaction().commit();
+                System.out.println("Категория " + categoryName + "успешно добавлена");
+            } catch (Exception e) {
+                manager.getTransaction().rollback();
+                System.out.println("Ошибка при добавлении новой категории " + e.getMessage());
+            }
+        } else {
+            System.out.println("Категория " + categoryName + " уже существует");
         }
-
-
     }
 }
+
